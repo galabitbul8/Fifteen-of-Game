da471253dc9aa4132373416e172b73bb193eb8d2
David Gal Abitbul <52243163+galabitbul8@users.noreply.github.com>
2021-03-17 23:35:30 +0200

    base

---
+++ FifteenofGame/app/src/main/java/com/galab/fifteen_of_game/GameActivity.java
@@ -0,0 +1,13 @@
+package com.galab.fifteen_of_game;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
