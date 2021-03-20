f9657990bd5674716ca0538119304863ec9399e9
David Gal Abitbul <52243163+galabitbul8@users.noreply.github.com>
2021-03-20 23:30:28 +0200

    add time

--- app/src/main/java/com/galab_rotemle/ex1/GameActivity.java
+++ app/src/main/java/com/galab_rotemle/ex1/GameActivity.java
@@ -2,24 +2,60 @@ package com.galab_rotemle.ex1;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
+import android.widget.TextView;
 
 import java.sql.Timestamp;
+import java.util.Locale;
 
 public class GameActivity extends AppCompatActivity implements View.OnClickListener{
     int moves = 0;
     String time = "00:00";
-
+    TextView txtTime;
+    Thread thread;
+    boolean done = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        txtTime = (TextView) findViewById(R.id.time);
+        startTime();
     }
 
 
     @Override
     public void onClick(View v) {
 
+    }
+
+    private void startTime(){
+        if(thread == null) {
+            thread = new Thread(new Runnable() {
+                int sec = 0;
+                int m = 0;
+                @Override
+                public void run() {
+                    while (!done) {
+                        //count second
+                        sec++;
+                        if (sec == 60) {
+                            sec = 0;
+                            m++; // count minutes
+                        }
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                txtTime.setText(String.format(Locale.getDefault(), "%02d", m) + ":" + String.format(Locale.getDefault(), "%02d", sec));
+                            }
+                        });
+                        SystemClock.sleep(1000);
+                    }
+                    thread = null;
+                }
+            });
+            thread.start();
+        }
     }
 }
